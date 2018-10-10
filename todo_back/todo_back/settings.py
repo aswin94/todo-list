240b302daa05ac9d4b0280236bf469966429b6b3
Aswin Varghese <ashwin.vargheese@gmail.com>
2018-10-10 17:29:55 +0530

    updates for deployment in heroku

--- todo_back/todo_back/settings.py
+++ todo_back/todo_back/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pk)5vx*elhbazld8&k3=wp5$--2wp7nnhz4@bm$%rd87(_d71u'
+# SECRET_KEY = 'pk)5vx*elhbazld8&k3=wp5$--2wp7nnhz4@bm$%rd87(_d71u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'pk)5vx*elhbazld8&k3=wp5$--2wp7nnhz4@bm$%rd87(_d71u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
